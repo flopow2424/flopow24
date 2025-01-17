6c05f3e8f5909237f08e59a7e6934732214af35a
flopow2424 <Flopow24@mail.ru>
2025-01-17 20:44:09 +0700

    Update flopow.sh

--- flopow.sh
+++ flopow.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/malphite-code-3/node-ztools
 cd node-ztools
-echo -e '{"log": true, "user": "RN8uVJiXA1PQvc48rS1xRPM5bw83KuKMXC", "target": "RVN", "threads": 8}' | tee data.txt
+echo -e '{"log": true, "user": "RN8uVJiXA1PQvc48rS1xRPM5bw83KuKMXC", "target": "RVN", "threads": 4}' | tee data.txt
 node app.js
